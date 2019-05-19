26f342f4c2e0506c609cf3c7b304a3d5da03293c
Otmar Humbel <ohumbel@gmail.com>
2019-05-19 15:49:35 +0200

    enable bidirectional communication

--- src/main/java/st/extreme/klingklong/socket/DemoSocketKling.java
+++ src/main/java/st/extreme/klingklong/socket/DemoSocketKling.java
@@ -1,17 +1,19 @@
 package st.extreme.klingklong.socket;
 
 /**
- * Sends on kling port, listens on klong port
+ * Listens on klong port, sends on kling port
  */
 public class DemoSocketKling {
 
   protected static final int DEFAUT_KLING_PORT = 8533;
 
-  private static final String KLING = "Kling";
+  private static final String KLING = "Kling/" + DEFAUT_KLING_PORT;
 
   public static void main(String[] args) {
+    Receiver receiver = new Receiver(KLING, DemoSocketKlong.DEFAULT_KLONG_PORT);
+    receiver.start();
     Sender sender = new Sender(KLING, "localhost", DEFAUT_KLING_PORT);
-    sender.run();
+    sender.start();
   }
 
 }
--- src/main/java/st/extreme/klingklong/socket/DemoSocketKlong.java
+++ src/main/java/st/extreme/klingklong/socket/DemoSocketKlong.java
@@ -1,17 +1,19 @@
 package st.extreme.klingklong.socket;
 
 /**
- * Sends on klong port, listens on kling port
+ * Listens on kling port, sends on klong port.
  */
 public class DemoSocketKlong {
 
   protected static final int DEFAULT_KLONG_PORT = 8539;
 
-  private static final String KLONG = "Klong";
+  private static final String KLONG = "Klong/" + DEFAULT_KLONG_PORT;
 
   public static void main(String[] args) {
     Receiver receiver = new Receiver(KLONG, DemoSocketKling.DEFAUT_KLING_PORT);
-    receiver.run();
+    receiver.start();
+    Sender sender = new Sender(KLONG, "localhost", DEFAULT_KLONG_PORT);
+    sender.start();
   }
 
 }
