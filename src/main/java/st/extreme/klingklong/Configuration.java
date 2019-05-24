a7a83c16465e00a4c75619b196311a7552b50d55
Otmar Humbel <ohumbel@gmail.com>
2019-05-24 22:57:31 +0200

    adjust configuration

--- src/main/java/st/extreme/klingklong/Configuration.java
+++ src/main/java/st/extreme/klingklong/Configuration.java
@@ -1,12 +1,30 @@
 package st.extreme.klingklong;
 
+import java.net.InetAddress;
+
+/**
+ * The configuration of an endpoint.
+ */
 public interface Configuration {
 
-  public String getLocalHostName();
-
+  /**
+   * Get the local port for the listening socket.
+   * 
+   * @return the local port
+   */
   public int getLocalPort();
 
-  public String getRemoteHostName();
+  /**
+   * Get the remote host.
+   * 
+   * @return the remote host
+   */
+  public InetAddress getRemoteHost();
 
+  /**
+   * Get the remote port for the sending socket.
+   * 
+   * @return the remote port
+   */
   public int getRemotePort();
 }
