5a4a6090571c1c037b4abd5e0338891337fc9854
Otmar Humbel <ohumbel@gmail.com>
2019-06-06 22:17:20 +0200

    switch to java.util.logging, using a very minimalistic formatter

--- src/main/java/st/extreme/klingklong/util/Horn.java
+++ src/main/java/st/extreme/klingklong/util/Horn.java
@@ -1,8 +1,29 @@
 package st.extreme.klingklong.util;
 
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 public final class Horn {
 
-  public static void honk(String message) {
-    System.out.println(message);
+  private static final Logger LOGGER;
+
+  static {
+    LOGGER = Logger.getLogger(Horn.class.getName());
+    // set our formatter on the console handler of the root logger
+    Logger.getLogger("").getHandlers()[0].setFormatter(new OneLineFormatter());
   }
+
+  public static void honk(String message) {
+    LOGGER.log(Level.INFO, message);
+  }
+
+  private static class OneLineFormatter extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+      return String.format("%s\n", record.getMessage());
+    }
+  }
+
 }
