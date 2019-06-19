49d094aaec8e8e5212bab7717e4cd601180175f8
Otmar Humbel <ohumbel@gmail.com>
2019-06-19 06:34:08 +0200

    improve javadoc

--- src/main/java/st/extreme/klingklong/util/FormattingListener.java
+++ src/main/java/st/extreme/klingklong/util/FormattingListener.java
@@ -5,7 +5,7 @@ public interface FormattingListener {
   /**
    * This method is called at the time a message is being formatted.
    * 
-   * @param formatted
+   * @param formatted The already formatted message
    */
   void onFormatting(String formatted);
 
